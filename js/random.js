67fcc1f9b035379c6c8a50fef5953fa1ed2d2a8d
Sktry <2580213579@qq.com>
2023-04-04 20:02:40 +0800

    Site updated: 2023-04-04 20:02:39

--- js/random.js
+++ js/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/04/04/hello-world/","2023/04/04/test-my-site/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
+var posts=["2023/04/04/test-my-site/","2023/04/04/hello-world/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
